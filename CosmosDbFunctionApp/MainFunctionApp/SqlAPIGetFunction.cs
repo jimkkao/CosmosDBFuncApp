894b47a6b6741f9f5c84f09e265002a8309cb626
Jim Kao <jikao@microsoft.com>
2019-09-26 13:29:42 -0700

    update route with uniqueid

--- CosmosDbFunctionApp/MainFunctionApp/SqlAPIGetFunction.cs
+++ CosmosDbFunctionApp/MainFunctionApp/SqlAPIGetFunction.cs
@@ -18,20 +18,18 @@ namespace MainFunctionApp
 
         [FunctionName("SqlAPIGetFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "customer/{uniqueid}")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var id = "{uniqueid}";
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+      
+            return (ActionResult)new OkObjectResult(id);
+
         }
     }
 }
