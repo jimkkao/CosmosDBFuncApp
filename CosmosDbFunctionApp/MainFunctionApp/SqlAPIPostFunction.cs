9d4f3cc8faa4673e4f29880b739dea56ad6ce834
Jim Kao <jikao@microsoft.com>
2019-10-11 14:13:16 -0700

    update

--- CosmosDbFunctionApp/MainFunctionApp/SqlAPIPostFunction.cs
+++ CosmosDbFunctionApp/MainFunctionApp/SqlAPIPostFunction.cs
@@ -14,7 +14,7 @@ namespace MainFunctionApp
     {
         [FunctionName("SqlAPIPostFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "customer")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
